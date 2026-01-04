d93599224401d283437368bf703606fde96d3316
spardeshi24 <sayalipardeshi240@gmail.com>
2026-01-04 21:46:25 +0530

    Created this module for calculating area.

---
+++ Week1.py
@@ -0,0 +1,30 @@
+def area_circle():
+    print("!...AREA OF CIRCLE...!")
+    r=float(input("enter radius:"))
+    area=3.14*r*r
+    print("area of circle=",area)
+
+
+def area_square():
+    print("!...AREA OF SQUARE...!")
+    s=float(input("enter side:"))
+    area=s*s
+    print("area of square=",area)
+
+
+def area_rectangle():
+    print("!...AREA OF RECTANGLE...!")
+    l=float(input("enter length of reactangle: "))
+    b=float(input("enter breadth of reactangle: "))
+    area=l*b
+    print("area of reactangle=",area)
+
+
+def area_triangle():
+    print("!...AREA OF TRIANGLE...!")
+    h=float(input("enter height of triangle: "))
+    b=float(input("enter breadth of triangle: "))
+    area=h*b
+    print("area of triangle=",area)
+    
+    
